82c4bb9a66e1ffaa6983cc6cb9b2a2368956269f
Kevin-C-Silva <kevin.silva31@aluno.cps.sp.gov.br>
2026-08-28 12:17:48 -0300

    Adicionando conteúdo à página principal e criando navegação para a página de filme(ainda com erro)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trabalho_chapeuzinho/filme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,53 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: PagInicial(),
     );
+  }
+}
+
+class PagInicial extends StatelessWidget {
+  const PagInicial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.lightGreen.shade100,
+        appBar: AppBar(
+          title: const Text("Sobre Deu a Louca na Chapeuzinho"),
+          backgroundColor: Colors.red.shade700,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  FloatingActionButton(
+                    backgroundColor: Colors.blueGrey,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Filme(),
+                          ),
+                      );
+                    },
+                    child: Text("Sobre o filme"),
+                  ),
+
+                  FloatingActionButton(
+                    backgroundColor: Colors.blueGrey,
+                    onPressed: null,
+                    child: Text("Personagens"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
   }
 }
